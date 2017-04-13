17fc907476d9f72f2fba8c5dfc948ea08b65e68f
shixi_keqi <shixi_keqi@staff.weibo.com>
2017-04-13 15:18:48 +0800

    Initial Commit

---
+++ LRU/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  LRU
+//
+//  Created by 宋珂琦 on 2017/4/13.
+//  Copyright © 2017年 宋珂琦. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
